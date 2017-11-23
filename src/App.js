4c7725d9eba047dd43f9446d64cfbbdd40002cfa
oscarjes <oscar@jesionek.co>
2017-11-23 19:30:37 +0700

    milestone 2

--- src/App.js
+++ src/App.js
@@ -3,12 +3,29 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+    }
+  }
+
+  toggleTitle = () => {
+    const newState = this.state.title ? null : 'Toggled state';
+    this.setState({
+      title: newState
+    });
+  }
+
   render() {
+    console.log("Good Evening!");
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <a href="#" onClick={this.toggleTitle}>
+            <img src={logo} className="App-logo" alt="logo" />
+          </a>
+          <h1 className="App-title">{this.state.title || this.props.title}</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
